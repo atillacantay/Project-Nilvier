2f1ab71d5e038f8932041259ac2723f741131252
Atilla Cantay Gül <atillacantay@hotmail.com>
2020-06-25 14:11:49 +0300

    Updates

---
+++ precache-manifest.fa401ed6f917e3b3ababfe68836a1659.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b572ee6dc7e60d1d3d1a9737c268d8d",
+    "url": "/Project-Nilvier/index.html"
+  },
+  {
+    "revision": "e00fb3fdee4c44f1dc63",
+    "url": "/Project-Nilvier/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e25731b4cd41513386d8",
+    "url": "/Project-Nilvier/static/js/2.425c8921.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/Project-Nilvier/static/js/2.425c8921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e00fb3fdee4c44f1dc63",
+    "url": "/Project-Nilvier/static/js/main.6a7ffc66.chunk.js"
+  },
+  {
+    "revision": "bb46bec9ca1087be7733",
+    "url": "/Project-Nilvier/static/js/runtime-main.b3dd0ff8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Project-Nilvier/static/media/logo.5d5d9eef.svg"
+  }
+]);
