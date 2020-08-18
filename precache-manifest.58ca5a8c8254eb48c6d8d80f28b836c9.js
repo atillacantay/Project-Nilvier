91551f86cbf2ff19e6067fd2d5b5b30951eb44f1
atillacantay <atillacantay@hotmail.com>
2020-08-18 22:29:55 +0300

    Updates

---
+++ precache-manifest.58ca5a8c8254eb48c6d8d80f28b836c9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f844f8640fdd6ca8eccc5605c9b94af9",
+    "url": "/Project-Nilvier/index.html"
+  },
+  {
+    "revision": "4783ec5b575e49dd3ef1",
+    "url": "/Project-Nilvier/static/css/main.45ae9166.chunk.css"
+  },
+  {
+    "revision": "562a07b43b906ae79ff8",
+    "url": "/Project-Nilvier/static/js/0.324181ca.chunk.js"
+  },
+  {
+    "revision": "c4dd13c18ed0635da90e",
+    "url": "/Project-Nilvier/static/js/1.ab8dfc13.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/Project-Nilvier/static/js/1.ab8dfc13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e0793b6dda2c07779a2",
+    "url": "/Project-Nilvier/static/js/10.e220ad9d.chunk.js"
+  },
+  {
+    "revision": "1db3558b3a30179118a1",
+    "url": "/Project-Nilvier/static/js/11.16e0a9bc.chunk.js"
+  },
+  {
+    "revision": "10e55bca6dbc58474d60",
+    "url": "/Project-Nilvier/static/js/12.77ee0d0c.chunk.js"
+  },
+  {
+    "revision": "42e3e93f2fa070d7f4d8",
+    "url": "/Project-Nilvier/static/js/13.f2caddb2.chunk.js"
+  },
+  {
+    "revision": "b11f4fa68b940a562376",
+    "url": "/Project-Nilvier/static/js/4.ebe517ea.chunk.js"
+  },
+  {
+    "revision": "276ddf9842587b4f3d449e1253efa054",
+    "url": "/Project-Nilvier/static/js/4.ebe517ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94cbebbb52fb4878b0c2",
+    "url": "/Project-Nilvier/static/js/5.8d2eb9c5.chunk.js"
+  },
+  {
+    "revision": "d6599ef5f6ad811e8752",
+    "url": "/Project-Nilvier/static/js/6.d9ad181f.chunk.js"
+  },
+  {
+    "revision": "db1c6fdc95bc4506883c",
+    "url": "/Project-Nilvier/static/js/7.a80f9bd0.chunk.js"
+  },
+  {
+    "revision": "776730a90d9622374bf9",
+    "url": "/Project-Nilvier/static/js/8.7abbb1d8.chunk.js"
+  },
+  {
+    "revision": "e28135f3ee193fb71fde",
+    "url": "/Project-Nilvier/static/js/9.0267c6d7.chunk.js"
+  },
+  {
+    "revision": "4783ec5b575e49dd3ef1",
+    "url": "/Project-Nilvier/static/js/main.abc61718.chunk.js"
+  },
+  {
+    "revision": "6a90a26e1ee6fc9cc67e",
+    "url": "/Project-Nilvier/static/js/runtime-main.df3c77f5.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Project-Nilvier/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8bf262b2e8f507e577dc0e0bd1435d7a.js"
+  "/Project-Nilvier/precache-manifest.58ca5a8c8254eb48c6d8d80f28b836c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Project-Nilvier/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
