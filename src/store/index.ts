bc69d57987072d50f4f362b68796140238fccb33
Atilla Cantay Gül <affectus@windowslive.com>
2020-10-12 00:20:08 +0300

    Updated base reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,20 @@
+import { combineReducers } from 'redux'
+
+import { authReducer } from './auth/reducers'
 import { giphyReducer } from './giphy/reducers'
 import { fooditiveReducer } from './fooditive/reducers'
 import { twitchReducer } from './twitch/reducers'
-
-import { combineReducers } from 'redux'
 import { movieReducer } from './movie/reducers'
+import { globalReducer } from './global/reducers'
+
 
 export const rootReducer = combineReducers({
+  auth: authReducer,
   giphy: giphyReducer,
   fooditive: fooditiveReducer,
   twitch: twitchReducer,
   movie: movieReducer,
+  global: globalReducer,
 })
 
 export type RootState = ReturnType<typeof rootReducer>
